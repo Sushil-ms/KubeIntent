c01e3915c04baeb3a3d38cb6b9262947a74d1ebc
Sushil-ms <sushilbala763@gmail.com>
2026-05-04 09:06:47 -0500

    Fix Kubernetes setup script for Docker Desktop

--- lib/k8s/client.ts
+++ lib/k8s/client.ts
@@ -1,3 +1,7 @@
-export function getKubernetesClient(): null {
-  return null;
-}
+import * as k8s from "@kubernetes/client-node";
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+export const k8sAppsV1 = kc.makeApiClient(k8s.AppsV1Api);
+export const k8sCoreV1 = kc.makeApiClient(k8s.CoreV1Api);
